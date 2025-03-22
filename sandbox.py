d0c98d1829e54741784e99be26ddc67da1c3fd25
AleksandarAngelevski <aleksandar.angelevski13@gmail.com>
2025-03-22 17:21:31 +0100

    Zadacha 1 od kolokviumski zadachi

---
+++ sandbox.py
@@ -0,0 +1,7 @@
+state =("h1","h2")
+
+ho = state[0],state[1]
+actions = {"Gore 1":(0,1),"Gore 2":(0,2),"Gore 3":(0,3),"Desno 1":(1,0),"Desno 2":(2,0),"Desno 3":(3,0),}
+for key in actions.keys():
+    print(key)
+    
